747d77bfe20fe03d009d499f9ebf2e11708f10da
lancebenedictos <lancebenedictos@gmail.com>
2024-04-11 13:31:26 -0400

    nav fix

--- src/components/nav/MobileNav.tsx
+++ src/components/nav/MobileNav.tsx
@@ -1,14 +1,15 @@
 import { Squash as Hamburger } from "hamburger-react";
 import { useState } from "react";
 import { NavSection } from "./DesktopNavButton";
-import { Section } from "../../store/store";
-import { motion } from "framer-motion";
+import { Section, useNavStore } from "../../store/store";
+import { motion, AnimatePresence } from "framer-motion";
 type Props = {
   navItems: NavSection[];
 };
 const MobileNav = ({ navItems }: Props) => {
   const [isOpen, setOpen] = useState(false);
-
+  const currentSection = useNavStore((state) => state.currentSection);
+  const isAbout = currentSection === "about";
   const onButtonClick = (section: Section) => {
     setOpen(false);
     const element = document.getElementById(section);
@@ -33,27 +34,31 @@ const MobileNav = ({ navItems }: Props) => {
           onToggle={() => {
             setOpen(!isOpen);
           }}
+          color={`${isAbout ? "white" : "black"}`}
         />
       </div>
 
-      {isOpen && (
-        <motion.div
-          initial={{ y: "100vh" }}
-          animate={{ y: "0" }}
-          transition={{ duration: 0.5, type: "spring" }}
-          className=" fixed top-0 left-0 inset-0 bg-red-200 -z-10 flex flex-col items-center pt-[80px] gap-3"
-        >
-          {navItems.map((nav) => (
-            <button
-              className=" font-bold text-xl"
-              key={nav.section}
-              onClick={() => onButtonClick(nav.section)}
-            >
-              {nav.label}
-            </button>
-          ))}
-        </motion.div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ y: "100vh" }}
+            animate={{ y: "0" }}
+            exit={{ y: "100vh" }}
+            transition={{ duration: 0.5, type: "spring" }}
+            className=" fixed top-0 left-0 inset-0  -z-10 flex flex-col bg-white items-center pt-[80px] gap-3"
+          >
+            {navItems.map((nav) => (
+              <button
+                className=" font-bold text-xl"
+                key={nav.section}
+                onClick={() => onButtonClick(nav.section)}
+              >
+                {nav.label}
+              </button>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
